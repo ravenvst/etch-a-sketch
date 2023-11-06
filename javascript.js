25109aeef749aac74bdc89af409be351ed671b88
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-11-06 17:59:02 +0100

    add 16 divs

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-const div = document.createElement("div");
+const container = document.querySelector(".container");
+
+function addDiv() {  
+    const div = document.createElement("div");
+    container.appendChild(div);
+}
+
+for (let i = 0; i < 16; i++){
+    addDiv();
+}
