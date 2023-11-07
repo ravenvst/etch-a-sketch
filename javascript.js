5ce3c5ac4ce0bbe441b44a5929b33d0d3641e40a
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-11-07 14:10:26 +0100

    add button function of adjusting the number of divs

--- javascript.js
+++ javascript.js
@@ -1,16 +1,32 @@
 const container = document.querySelector(".container");
+let number = 16;
+const button = document.querySelector("button");
+button.addEventListener("click", requestNumber);  // THIS WORKS
 
-for (let i = 0; i < 256; i++){
-    addDiv();
+createSketchpad();
+
+function createSketchpad() {
+    for (let i = 0; i < number*number; i++){
+        addDiv();
+    }
+    
+    function addDiv() {  
+        const div = document.createElement("div");
+        container.appendChild(div);
+    }
 }
 
-function addDiv() {  
-    const div = document.createElement("div");
-    container.appendChild(div);
-}
+
+
 
 function requestNumber () {
-    let number = prompt("Please enter the number of squares");
+ 
+    let list = document.querySelectorAll("div");
+    list.forEach(function(node){
+        node.remove();
+    })
+    number = prompt("Please enter the number of squares per line (max 100)");
+    createSketchpad();
 }
 
 
