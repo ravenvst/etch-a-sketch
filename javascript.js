3c44497c580ce2508ecd1f7525735f5fbcf4cde9
ravenvst <142888381+ravenvst@users.noreply.github.com>
2023-11-07 12:37:31 +0100

    add color changing on hover

--- javascript.js
+++ javascript.js
@@ -1,10 +1,14 @@
 const container = document.querySelector(".container");
 
+for (let i = 0; i < 256; i++){
+    addDiv();
+}
+
 function addDiv() {  
     const div = document.createElement("div");
     container.appendChild(div);
 }
 
-for (let i = 0; i < 256; i++){
-    addDiv();
-}
+
+
+
